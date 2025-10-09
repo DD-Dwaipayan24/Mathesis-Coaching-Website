536962d0ce404940d487d3d9dd48a481f563d28b
Dwaipayan_Dutta_18 <codeher2406@gmail.com>
2025-10-08 22:07:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 
 // Middleware
